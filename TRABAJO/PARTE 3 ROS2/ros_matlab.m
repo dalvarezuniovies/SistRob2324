f75f8c38713708fa8263085e8d1af0e07e2da412
uo296111 <uo296111@uniovi.es>
2024-01-10 09:25:52 +0100

    Update ros_matlab.m

--- TRABAJO/PARTE 3 ROS2/ros_matlab.m
+++ TRABAJO/PARTE 3 ROS2/ros_matlab.m
@@ -1,15 +1,13 @@
 %conectar robola2g 74269891
-% nodo = ros2node("/nodo_inicial");
-% ros2 node list
-% ros2 topic list
-%chatterSub = ros2subscriber(nodo,"/cmd_vel") %suscribirse al topic chatter
-%mensaje_testSub = receive(chatterSub,10) %leer cada 10s
-chatterPub = ros2publisher(nodo,"/turtle1/cmd_vel","geometry_msgs/Twist") %crear publisher
+ nodo = ros2node("/nodo_navegacion");
+chatterPub = ros2publisher(nodo,"/goal_pose","geometry_msgs/PoseStamped") %crear publisher
 % Creamos un mensaje y lo rellenamos 
 chatterMsg = ros2message(chatterPub)
-chatterMsg.linear.x =1.0
-%chatterMsg.angular='[0.0, 0.0, 0.0]'
-send(chatterPub,chatterMsg) %envío mensaje 
+chatterMsg.header.frame_id ='map'
+chatterMsg.pose.position.x=0.106
+chatterMsg.pose.position.y=-2.52
+chatterMsg.pose.orientation.w=1
+%send(chatterPub,chatterMsg) %envío mensaje 
 %ros2 topic list -t tipo mensajes
 
 
